50777967cbfc27d9b4bfe254c387804ade57c591
Keiichi Kii <kkii@redhat.com>
2023-03-14 23:32:00 -0400

    test

--- controllers/selfnoderemediation_controller_test.go
+++ controllers/selfnoderemediation_controller_test.go
@@ -402,7 +402,7 @@ func isTaintExist(taintToMatch *v1.Taint) (bool, error) {
 		return false, err
 	}
 	for _, taint := range node.Spec.Taints {
-		fmt.Printf("isTaintExist() check1 taint key:%s effect:%s\n", taint.Key, taint.TaintEffect)
+		fmt.Printf("isTaintExist() check1 taint key:%s effect:%s\n", taint.Key, taint.Effect)
 		if taintToMatch.MatchTaint(&taint) {
 			return true, nil
 		}
